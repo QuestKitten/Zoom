bb55a4031304dfafdc1f7deeda44fcf62c8c74ac
Quest <148282963+QuestKitten@users.noreply.github.com>
2023-11-02 02:33:14 +0900

    Update DiamondCalculator.py

--- DiamondCalculator.py
+++ DiamondCalculator.py
@@ -5,9 +5,25 @@ Option = input("how may I assist you today? (Diamonds/Levels): ")
 
 #seperate Diamonds and Levels
 if Option == "Diamonds":
+ 
   print("Diamonds selected.")
-  DiamondInput = input("How many are you looking to obtain? ")
+  
+  DiamondInput = int(input("How many are you looking to obtain? "))
+  if DiamondInput <= 100000:
+    print("I see, a small goal...")
+  elif DiamondInput <= 975000:
+    print("Wonderful, a goal worth working towards!")
+  elif DiamondInput >= 975001:
+    print("Holy cheese sticks! WHAT DO YOU EVEN NEED THAT MANY FOR?!)
+    print("I believe I will be needing more coffee before we continue...")
+    print(". . .")
+    print("okay, let's continue.")
+          
+  Gamepass = input("what gamepass do you own? (none/x2/x4/x6) ")
+  if Gamepass == "x2":
   DiamondInput = DiamondInput *
+
+
   # WIP
   # Diamonds per level on line 13
   # 300dims no gamepass / 600dims x2 gamepass / 1200dims x4 gamepass / 1800dims both gamepasses
