c507861b471677e681315d8ec499c9df8d97f739
Quest <148282963+QuestKitten@users.noreply.github.com>
2023-11-01 15:21:36 +0900

    Create DiamondCalculator.py

---
+++ DiamondCalculator.py
@@ -0,0 +1,18 @@
+# Caclulate how many levels Nephi needs to obtain "x" many diamonds
+# Calculate what Nephi can do to obtain "x" levels most effectivly
+print("Welcome back Nephi!)
+Option = input("how may I assist you today? (Diamonds/Levels): ")
+
+#seperate Diamonds and Levels
+if Option == "Diamonds":
+  print("Diamonds selected.")
+  DiamondInput = input("How many are you looking to obtain? ")
+  DiamondInput = DiamondInput *
+  # WIP
+  # Diamonds per level on line 13
+  # 300dims no gamepass / 600dims x2 gamepass / 1200dims x4 gamepass / 1800dims both gamepasses
+
+# Exp needed to level up: 325
+# 240 exp from eating ( 0% - 100% )
+# 200 exp from sleeping ( 0% - 100% )
+# 150 exp from showering ( 0% - 100% )
