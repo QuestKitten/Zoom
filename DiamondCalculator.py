31af383d6eb8eb98b78914e00d2adfa7732900d1
Quest <148282963+QuestKitten@users.noreply.github.com>
2023-11-02 03:51:16 +0900

    DiamondCalculator.py

--- DiamondCalculator.py
+++ DiamondCalculator.py
@@ -1,27 +1,32 @@
 # Caclulate how many levels Nephi needs to obtain "x" many diamonds
 # Calculate what Nephi can do to obtain "x" levels most effectivly
-print("Welcome back Nephi!)
-Option = input("how may I assist you today? (Diamonds/Levels): ")
+print("Welcome back Nephi!")
+Option = input("how may I assist you today? (Diamonds/Levels): ").lower()
 
 #seperate Diamonds and Levels
-if Option == "Diamonds":
+if Option == "diamonds":
  
   print("Diamonds selected.")
   
   DiamondInput = int(input("How many are you looking to obtain? "))
-  if DiamondInput <= 100000:
+  if DiamondInput <= -1:
+    print("What the hell are you doing?")
+    print("Just give away your diamonds you fool.")
+  elif DiamondInput <= 100000:
     print("I see, a small goal...")
   elif DiamondInput <= 975000:
     print("Wonderful, a goal worth working towards!")
   elif DiamondInput >= 975001:
-    print("Holy cheese sticks! WHAT DO YOU EVEN NEED THAT MANY FOR?!)
+    print("Holy cheese sticks! WHAT DO YOU EVEN NEED THAT MANY FOR?!")
     print("I believe I will be needing more coffee before we continue...")
     print(". . .")
     print("okay, let's continue.")
+
+
           
-  Gamepass = input("what gamepass do you own? (none/x2/x4/x6) ")
-  if Gamepass == "x2":
-  DiamondInput = DiamondInput *
+#  Gamepass = input("what gamepass do you own? (none/x2/x4/x6) ")
+#  if Gamepass == "x2":
+#  DiamondInput = DiamondInput *
 
 
   # WIP
