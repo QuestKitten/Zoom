10dc6e849ddf21049bcf2041fdcfa1e82caa1f2c
Quest <148282963+QuestKitten@users.noreply.github.com>
2023-11-17 15:22:20 +0900

    Update DiamondCalculator.py

--- DiamondCalculator.py
+++ DiamondCalculator.py
@@ -1,39 +1,129 @@
-# Caclulate how many levels Nephi needs to obtain "x" many diamonds
-# Calculate what Nephi can do (shower/sleep/eat) & how many times each to obtain "x" levels most effectivly
-print("Welcome back Nephi!")
+# Caclulate how many levels *USER* needs to obtain "x" many diamonds
+# Calculate what *USER* can do (shower/sleep/eat) & how many times each to obtain "x" levels most effectivly
+User = input("Please enter your name: ")
+print(f"Welcome back {User}")
 Option = input("how may I assist you today? (Diamonds/Levels): ").lower()
 
+#Defining variables
+DiamondsFromLeveling = 300
+ExperienceNeededPerLevel = 325
+
+
+
+
 #seperate Diamonds and Levels
+
+#Diamonds breakdown
 if Option == "diamonds":
- 
-  print("Diamonds selected.")
-  
-  DiamondInput = int(input("How many are you looking to obtain? "))
-  if DiamondInput <= -1:
-    print("What the hell are you doing?")
-    print("Just give away your diamonds you fool.")
-  elif DiamondInput <= 100000:
-    print("I see, a small goal...")
-  elif DiamondInput <= 975000:
-    print("Wonderful, a goal worth working towards!")
-  elif DiamondInput >= 975001:
-    print("Holy cheese sticks! WHAT DO YOU EVEN NEED THAT MANY FOR?!")
-    print("I believe I will be needing more coffee before we continue...")
-    print(". . .")
-    print("okay, let's continue.")
+    print("Diamonds selected.")
 
 
-          
-#  Gamepass = input("what gamepass do you own? (none/x2/x4/x6) ")
-#  if Gamepass == "x2":
-#  DiamondInput = DiamondInput *
+    while Option == "diamonds":
+        
+            DiamondInput = int(input("How many are you looking to obtain? (Please enter a whole number): "))
+            if DiamondInput <= -1:
+                print("What the hell are you doing?\nJust give away your diamonds you fool.")
+                Diamonds = DiamondInput
+                continue
+
+            elif DiamondInput <= 100000:
+                print("I see, a small goal...")
+                Diamonds = DiamondInput
+                break
+
+            elif DiamondInput <= 975000:
+                print("Wonderful, a goal worth working towards!")
+                Diamonds = DiamondInput
+                break
+
+            elif DiamondInput >= 975001:
+                print("Holy cheese sticks! WHAT DO YOU EVEN NEED THAT MANY FOR?! \nI believe I will be needing more coffee before we continue...\n. . .\nOkay, let's continue.")
+                Diamonds = DiamondInput
+                break
+
+            #same as line 72 and line 110
+            else:
+                print("How did you manage to break the code this bad...")
+                break
+        
+
+        #Loop in gamepass selection
+    if Option == "diamonds":
+        Gamepass = input("what gamepass do you own? (none/x2/x4/both): ")
+        while Option == "diamonds":
+
+            if Gamepass == "x2":
+                DiamondsFromLeveling *= 2
+                break
+
+            elif Gamepass == "x4":
+                DiamondsFromLeveling *= 4
+                break
+
+            elif Gamepass == "both":
+                DiamondsFromLeveling *= 6
+                break
+
+            elif Gamepass == "none":
+                DiamondsFromLeveling *= 1
+                break
+                
+            #need to fix so it doesnt print infinite
+            else:
+                print("Please try again...")
+                
 
 
-  # WIP
-  # Diamonds per level on line 13
-  # 300dims no gamepass / 600dims x2 gamepass / 1200dims x4 gamepass / 1800dims both gamepasses
 
-# Exp needed to level up: 325
-# 240 exp from eating ( 0% - 100% )
-# 200 exp from sleeping ( 0% - 100% )
-# 150 exp from showering ( 0% - 100% )
+        #Calculate number of levels
+        LevelsNeeded = Diamonds // DiamondsFromLeveling
+
+        #EXP per activity
+        EXP_from_eating = 240
+        EXP_from_sleeping = 200
+        EXP_from_showering = 150
+
+        #number of times to do each activity
+        NumberTimesForEating = LevelsNeeded * ExperienceNeededPerLevel / EXP_from_eating
+        NumberTimesForSleeping = LevelsNeeded * ExperienceNeededPerLevel / EXP_from_sleeping
+        NumberTimesForShowering =LevelsNeeded * ExperienceNeededPerLevel / EXP_from_showering
+        
+
+        #Choice of action
+        ActionChoice = input("Which way do you prefer to gain diamonds? (eat/sleep/bathe): ").lower()
+        while Option == "diamonds":
+
+            if ActionChoice == "eat":
+                NumberOfFoodItemsNeeded = NumberTimesForEating * 4
+                print(f"{User}, you would need to eat {round(NumberOfFoodItemsNeeded) + 1} food items.")
+                break
+
+            elif ActionChoice == "sleep":
+                print(f"{User}, you would need to fill your energy bar, from 0% - 100%, {round(NumberTimesForSleeping) + 1} times.")
+                break
+
+            elif ActionChoice == "bathe":
+                print(f"{User}, you would need to fill your hygiene bar, from 0%-100%, {round(NumberTimesForShowering) +1} times.")
+                break
+                
+            #need to fix so it doesnt print infinite
+            else:
+                print("I don't understand your request, please try again.")
+
+
+
+
+    #Levels breakdown
+elif Option == "levels":
+    print("Levels selected.")
+
+    #while Option == "levels":
+
+
+
+
+
+
+    #Failure breakdown
+else:
+    print("Try again or do us all a favour leave and never come back. <3")
